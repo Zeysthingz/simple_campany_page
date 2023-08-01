89fd32e60adf0d6fc6a42ab9638af839a832599a
Zeynep <zeynepakbalikk@gmail.com>
2023-08-01 22:56:30 +0300

    index page added

--- page/views.py
+++ page/views.py
@@ -11,6 +11,6 @@ def index(request):
     # print(request.META)
     # print(request.HEADER)
     # return HttpResponse("Hello, world. You're at the polls index.")
+    context={}
 
-    return render(request, 'page/index.html',
-                  {'hello': f"Hello, world. You\'re at the polls index {randint(0, 1000)}."})
+    return render(request, 'page/index.html',context)
