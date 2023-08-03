64d2862d619efab6361b1ee1033109ac47b79c55
Zeynep <zeynep@leodrive.ai>
2023-08-03 23:31:34 +0300

    used DTL for

--- page/views.py
+++ page/views.py
@@ -7,6 +7,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+FAKE_DATABASE = [
+    f"https://picsum.photos/id/{id}/1200/600 " for id in range(21, 29)
+]
+
+
 def index(request):
     # print(request.META)
     # print(request.HEADER)
@@ -15,14 +21,17 @@ def index(request):
     context = {
         "page_title": page_title,
     }
-    print(context)
     return render(request, 'page/index.html', context)
 
 
 def about(request):
     page_title="About"
+    hero_title = "About Us!"
+    hero_text= "At the heart of TechDjango Solutions beats a team of passionate developers, designers"
     context = {
         "page_title": page_title,
+        "hero_title": hero_title,
+        "hero_text": hero_text,
     }
     return render(request, 'page/about.html', context)
 
@@ -34,5 +43,6 @@ def contact(request):
         "page_title": page_title,
         "hero_title": hero_title,
         "hero_text": hero_text,
+        "fake_database": FAKE_DATABASE,
     }
     return render(request, 'page/contact.html', context)
